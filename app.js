3c9d3bca69a539fe615f3afb0f61be56cb84c3fe
shinmetry <acr0ss.the.universe0105@gmail.com>
2018-09-29 04:33:17 +0000

    Herokuの環境変数を利用できるように修正

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:8000/auth/google/callback"
+  callbackURL: process.env.HEROKU_URL ? process.env.HEROKU_URL + 'auth/google/callback' : "http://localhost:8000/auth/google/callback"
 }, function (accessToken, refreshToken, profile, done) {
   process.nextTick(function () {
      done(null, profile);
